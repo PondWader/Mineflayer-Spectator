180019554e3721430af182e0416fad75f798ec57
PondWader <66561610+PondWader@users.noreply.github.com>
2023-09-28 16:47:21 +0100

    Fix chat in online mode servers

--- src/handlers/general.ts
+++ src/handlers/general.ts
@@ -14,4 +14,14 @@ export function registerGeneralListeners(bot: mineflayer.Bot, server: _Server, c
         // Blocking gamemode changes
         if (data.reason !== 3) server.writeAll('game_state_change', data)
     })
+
+    // Send all chat messages as system chat to avoid chat signing issues
+    server.excludePacketFromProxy('player_chat', 'system_chat', 'profileless_chat')
+    bot.on('message', (msg) => {
+        //const message = data.formattedMessage
+        server.writeAll('system_chat', {
+            content: JSON.stringify(msg.json),
+            isActionBar: false
+        })
+    })
 }
